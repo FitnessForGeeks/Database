b8505f6a57b0cc7d7bce0b3d13d90e8846c7e00a
TimUntersberger <timuntersberger2@gmail.com>
2018-04-20 12:08:37 +0200

    added sessions

--- setup.sql
+++ setup.sql
@@ -1,8 +1,14 @@
 create database if not exists FitnessForGeeks;
 use FitnessForGeeks;
-drop table accounts;
+drop table if exists accounts cascade;
+drop table if exists sessions;
+create table sessions(
+  id int(10) not null primary key auto_increment,
+  sessionKey varchar(64) unique not null,
+  connections int(2)
+);
 create table accounts(
-  id int not null primary key auto_increment,
+  id int(10) not null primary key auto_increment,
   username varchar(20) not null unique,
   password varchar(64) not null,
   first_name varchar(15),
@@ -13,5 +19,7 @@ create table accounts(
   height int(3),
   bmi decimal (3, 1),
   isVerified boolean not null default false,
-  description varchar(255)
+  sessionId int(10),
+  description varchar(255),
+  foreign key(sessionId) references sessions(id)
 );
