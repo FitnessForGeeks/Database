355dda9fdb9bf6e1ae35f764c09091e2cb44e7c7
TimUntersberger <timuntersberger2@gmail.com>
2018-06-12 09:48:34 +0200

    added recipes table

--- create.sql
+++ create.sql
@@ -1,25 +1,40 @@
 create database if not exists FitnessForGeeks;
 use FitnessForGeeks;
 
-create table accounts(
-  id int(10) not null primary key auto_increment,
-  username varchar(20) not null unique,
-  password varchar(64) not null,
-  firstName varchar(15),
-  lastName varchar(20),
-  isMale boolean,
-  email varchar(320) not null unique,
-  birthDate date,
-  weight double(5, 2),
-  height int(3),
-  isVerified boolean not null default false,
-  authKey varchar(64) not null unique,
-  description varchar(255)
+CREATE TABLE accounts (
+    id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(64) NOT NULL,
+    firstName VARCHAR(15),
+    lastName VARCHAR(20),
+    isMale BOOLEAN,
+    email VARCHAR(320) NOT NULL UNIQUE,
+    birthDate DATE,
+    weight DOUBLE(5 , 2 ),
+    height INT(3),
+    isVerified BOOLEAN NOT NULL DEFAULT FALSE,
+    authKey VARCHAR(64) NOT NULL UNIQUE,
+    description VARCHAR(255)
 );
 
-create table eatenRecipes(
-  id int(10) not null primary key auto_increment,
-  accountId int(10) not null references accounts(id),
-  date date not null,
-  recipeId int(10) not null
+CREATE TABLE eatenRecipes (
+    id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    accountId INT(10) NOT NULL REFERENCES accounts (id),
+    date DATE NOT NULL,
+    recipeId INT(10) NOT NULL
 );
+
+CREATE TABLE recipes (
+    id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL UNIQUE,
+    image VARCHAR(256),
+    calories INT(5)
+);
+
+insert into recipes(title, image, calories) values('Kuchen', 'https://de.rc-cdn.community.thermomix.com/recipeimage/jtvuiwjs-cf76f-438951-cfcd2-2nm5kkqw/b1db0880-a300-4618-ad14-18be177dfbe8/main/0815-kuchen-gugelhupf-mit-gelinggarantie.jpg', 2000);
+insert into recipes(title, image, calories) values('Keks', 'https://www.duden.de/_media_/full/K/Keks-201100285851.jpg', 739);
+insert into recipes(title, image, calories) values ('Pizza', 'http://www.delonghi.com/Global/recipes/multifry/pizza_fresca.jpg', 495);
+insert into recipes(title, image, calories) values ('Fisch', 'https://static.chefkoch-cdn.de/ck.de/rezepte/52/52527/792482-960x720-fisch-in-chilibutter-in-alufolie.jpg', 1630);
+insert into recipes(title, image, calories) values ('Nudeln', 'https://www.fuersie.de/sites/fuersie/files/styles/590x443/public/images/pasta.jpg', 1);
+
+update accounts set birthdate = str_to_date('15/4/2001', '%d/%m/%Y'), weight = 92, height = 182, isMale = true where id = 1;
--- drop.sql
+++ drop.sql
@@ -1,3 +1,4 @@
 use FitnessForGeeks;
 drop table if exists accounts;
 drop table if exists eatenRecipes;
+drop table if exists recipes;
