3a5f3c1df8543dd582cb34a0d0e1aeb9bb75b9f1
timun <user.name>
2018-06-17 20:17:51 +0200

    Add insert scripts for each table. Modify recipes table for reviews

--- create.sql
+++ create.sql
@@ -10,11 +10,12 @@ CREATE TABLE accounts (
     isMale BOOLEAN,
     email VARCHAR(320) NOT NULL UNIQUE,
     birthDate DATE,
-    weight DOUBLE(5 , 2 ),
+    weight DOUBLE(5 , 2),
     height INT(3),
     isVerified BOOLEAN NOT NULL DEFAULT FALSE,
     authKey VARCHAR(64) NOT NULL UNIQUE,
-    description VARCHAR(255)
+    description VARCHAR(255),
+    createdAt timestamp
 );
 
 CREATE TABLE eatenRecipes (
@@ -26,15 +27,23 @@ CREATE TABLE eatenRecipes (
 
 CREATE TABLE recipes (
     id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    accountId INT(10) REFERENCES accounts(id),
     title VARCHAR(30) NOT NULL UNIQUE,
+    description varchar(255),
     image VARCHAR(256),
-    calories INT(5)
+    directions text,
+    ingredients text,
+    calories INT(5),
+    reviewCount int(5) not null default 0,
+    createdAt timestamp,
+    avgRating double(3, 2) default 0
 );
 
-insert into recipes(title, image, calories) values('Kuchen', 'https://de.rc-cdn.community.thermomix.com/recipeimage/jtvuiwjs-cf76f-438951-cfcd2-2nm5kkqw/b1db0880-a300-4618-ad14-18be177dfbe8/main/0815-kuchen-gugelhupf-mit-gelinggarantie.jpg', 2000);
-insert into recipes(title, image, calories) values('Keks', 'https://www.duden.de/_media_/full/K/Keks-201100285851.jpg', 739);
-insert into recipes(title, image, calories) values ('Pizza', 'http://www.delonghi.com/Global/recipes/multifry/pizza_fresca.jpg', 495);
-insert into recipes(title, image, calories) values ('Fisch', 'https://static.chefkoch-cdn.de/ck.de/rezepte/52/52527/792482-960x720-fisch-in-chilibutter-in-alufolie.jpg', 1630);
-insert into recipes(title, image, calories) values ('Nudeln', 'https://www.fuersie.de/sites/fuersie/files/styles/590x443/public/images/pasta.jpg', 1);
-
-update accounts set birthdate = str_to_date('15/4/2001', '%d/%m/%Y'), weight = 92, height = 182, isMale = true where id = 1;
+create table reviews(
+	id INT(10) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    accountId INT(10) REFERENCES accounts(id),
+    recipeId INT(10) references recipes(id),
+    text text not null,
+    rating double(3, 2) not null check(rating >= 0 and rating <= 5),
+    createdAt timestamp not null
+);
--- drop.sql
+++ drop.sql
@@ -2,3 +2,4 @@ use FitnessForGeeks;
 drop table if exists accounts;
 drop table if exists eatenRecipes;
 drop table if exists recipes;
+drop table if exists reviews;
